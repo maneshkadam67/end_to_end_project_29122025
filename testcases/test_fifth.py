824615705ea01bcba9b084a3fe8c12510f2fdc92
maneshkadam67 <maneshkadam67@gmail.com>
2025-12-29 22:00:28 +0530

    added some changes

--- testcases/test_fifth.py
+++ testcases/test_fifth.py
@@ -1,13 +1,19 @@
 import pytest
 
 
-@pytest.mark.parametrize("status",[(300)])
-def test_status(status):
-    assert status==300
+@pytest.mark.parametrize("status",[(200)])
+def test_status(status,request,worker_id):
+    #print(f"this is workerid for method {request.node.name}-----------{worker_id}")
 
+    print("-------------------------Test name:", request.node.name)
+    print("-------------------------Node ID:", request.node.nodeid)
+    print("-------------------------File:", request.node.fspath)
+    print("worker_id--------------------",worker_id)
+    assert status == 200
 
-def test_assert():
-    assert True==1
+def test_assert(worker_id):
+    print("worker_id--------------------", worker_id)
+    assert True==""
 
 
 
